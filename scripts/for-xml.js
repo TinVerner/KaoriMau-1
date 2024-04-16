8934b304a1e4d7a1ed07928ad760975293a3478b
TinVerner <tinverner@gmail.com>
2024-04-17 00:00:46 +0300

    17.04.2024 00:00 +XML

--- scripts/for-xml.js
+++ scripts/for-xml.js
@@ -1,32 +1,32 @@
-// Загрузка XML-файла
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    parseXML(this);
-  }
-};
-xmlhttp.open("GET", "description.xml", true);
-xmlhttp.send();
+function displayАrticle(tagName) {
+  let xhr = new XMLHttpRequest();
 
-// Обработка XML
-function parseXML(xml) {
-  var xmlDoc = xml.responseXML;
-  var articles = xmlDoc.getElementsByTagName("article");
-  var articlesDiv = document.getElementById("articles");
+  xhr.open('GET', 'description.xml', true);
+  xhr.setRequestHeader('Content-Type', 'application/xml');
+  xhr.send();
 
-  // Создание элементов на основе данных XML
-  for (var i = 0; i < articles.length; i++) {
-    var article = articles[i];
-    var title = article.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-    var content = article.getElementsByTagName("content")[0].childNodes[0].nodeValue;
-    var author = article.getElementsByTagName("author")[0].childNodes[0].nodeValue;
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let xmlContent = xhr.responseXML;
+      let elements = xmlContent.getElementsByTagName(tagName);
 
-    var articleDiv = document.createElement("div");
-    articleDiv.innerHTML = "<h2>" + title + "</h2><p>" + content + "</p><p>Автор: " + author + "</p>";
-    articleDiv.classList.add("visible"); // Добавление класса .visible
-    articlesDiv.appendChild(articleDiv);
-  }
+      if (elements.length > 0) {
+        let title = elements[0].getElementsByTagName('title')[0].textContent;
+        let content = elements[0].getElementsByTagName('content')[0].textContent;
+
+        let titleElement = document.createElement('h1');
+        titleElement.textContent = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.textContent = content;
+
+        let articleElement = document.getElementById('article');
+        articleElement.innerHTML = '';
+        articleElement.appendChild(titleElement);
+        articleElement.appendChild(contentElement);
+      }
+    }
+  };
 }
-function redirectToIndex() {
-  window.location.href = "index.html";
-}
+
+displayАrticle('articles');
